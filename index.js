8dd4fb37d255755e7b9970ca934311bc6d9c6ff5
HungNV <hungnv@vitalify.asia>
2017-08-05 13:55:10 +0700

    #update file .gitignore

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 // Add middleware to handle post request for express
+app.set('port', (process.env.PORT || 5000));
 app.use('/static', express.static('static'));
 
 // Serve index.html for path '/', this is home path
@@ -13,8 +14,8 @@ app.get('/', (req, res) => {
 
 var trackingState = {}
 
-server.listen(8088, function(){
-    console.log('Dang lang nghe tren post 8088');
+server.listen(5000, function(){
+    console.log('Dang lang nghe tren post 5000');
     io.on('connection', function(socket){
         console.log('Co nguoi ket noi');
         
